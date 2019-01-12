f496985df8a412b8955b1d1868eabac048290cf6
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-12 20:14:42 +0100

    Finalizado punto 3

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.agenda;
+
+public class Contacto {
+	private static final String ER_TELEFONO;
+	private static final String ER_CORREO;
+	private String nombre;
+	private String telefono;
+	private String correo;
+
+}
