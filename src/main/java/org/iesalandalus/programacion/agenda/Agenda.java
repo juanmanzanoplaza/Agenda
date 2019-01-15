a51f67e49debdae46c4f65230e1fa5a31d3438c5
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-15 13:34:36 +0100

    Realizado punto 8 y añadido atributo contactos faltante en la clase Agenda

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -3,5 +3,23 @@ package org.iesalandalus.programacion.agenda;
 public class Agenda {
 	private static final int MAX_CONTACTOS = 100;
 	private int numContactos;
+	private Contacto[] contactos;
+	
+	public Agenda() {
+		numContactos = 0;
+		contactos = new Contacto[MAX_CONTACTOS];
+	}
+	
+	public Contacto[] getContactos() {
+		Contacto[] devolver = new Contacto[contactos.length];
+		for(int i = 0; i < contactos.length; i++) {
+			devolver[i] = contactos[i];
+		}
+		return devolver;
+	}
+	
+	public int getNumContactos() {
+		return numContactos;
+	}
 
 }
